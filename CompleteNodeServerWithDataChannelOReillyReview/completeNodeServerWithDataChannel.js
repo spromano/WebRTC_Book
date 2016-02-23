3ab160be34c825ca62551872a33d31c6c817c01a
spromano <spromano@unina.it>
2016-02-23 18:18:13 +0100

    Minor modifications
    Modified a couple of things in order to let the (old) code work with up-to-date browser versions. Major restructuring is needed in order to align the examples to the almost ready WebRTC1.0 spec. I'll do that in the upcoming second edition of the book :-)

--- CompleteNodeServerWithDataChannelOReillyReview/completeNodeServerWithDataChannel.js
+++ CompleteNodeServerWithDataChannelOReillyReview/completeNodeServerWithDataChannel.js
@@ -9,45 +9,22 @@ var app = http.createServer(function (req, res) {
   file.serve(req, res);
 }).listen(8181);
 
-console.log('Listening on ' + app.address().port);
-
 // Use socket.io JavaScript library for real-time web applications
 var io = require('socket.io').listen(app);
 
-
-// credits to http://stackoverflow.com/questions/6563885/socket-io-how-do-i-get-a-list-of-connected-sockets-clients/24145381#24145381
-function findClientsSocket(roomId, namespace) {
-    var res = [];
-    var ns = io.of(namespace ||"/");    // the default namespace is "/"
-
-    if (ns) {
-        for (var id in ns.connected) {
-            if(roomId) {
-                var index = ns.connected[id].rooms.indexOf(roomId) ;
-                if(index !== -1) {
-                    res.push(ns.connected[id]);
-                }
-            } else {
-                res.push(ns.connected[id]);
-            }
-        }
-    }
-    return res.length;
-}
-
 // Let's start managing connections...
 io.sockets.on('connection', function (socket){
-
+	
     	// Handle 'message' messages
         socket.on('message', function (message) {
                 log('S --> got message: ', message);
                 // channel-only broadcast...
                 socket.broadcast.to(message.channel).emit('message', message);
         });
-
+        
         // Handle 'create or join' messages
         socket.on('create or join', function (room) {
-                var numClients = findClientsSocket(room);
+                var numClients = io.sockets.clients(room).length;
 
                 log('S --> Room ' + room + ' has ' + numClients + ' client(s)');
                 log('S --> Request to create or join room', room);
@@ -57,7 +34,7 @@ io.sockets.on('connection', function (socket){
                         socket.join(room);
                         socket.emit('created', room);
                 } else if (numClients == 1) {
-                // Second client joining...
+                // Second client joining...                	
                         io.sockets.in(room).emit('join', room);
                         socket.join(room);
                         socket.emit('joined', room);
@@ -65,7 +42,7 @@ io.sockets.on('connection', function (socket){
                         socket.emit('full', room);
                 }
         });
-
+        
         function log(){
             var array = [">>> "];
             for (var i = 0; i < arguments.length; i++) {
